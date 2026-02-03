3206d15e0b4a574be6c8938a64c32aea46749c6c
Anushka_baliya <anushkabaliya27@gmail.com>
2026-02-03 22:41:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/FocusFlow-Hub-05/",
+  base: "/focusflow-hub-38/",
   server: {
     host: "::",
     port: 8080,
